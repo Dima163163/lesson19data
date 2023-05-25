25cb7791b61bd589344995528e73fc7800a6da65
Dima163163 <khlopovdima@gmail.com>
2023-05-25 16:46:07 +0400

    lesson19 сложное

---
+++ Desktop/Animation/script.js
@@ -0,0 +1,44 @@
+const block = document.querySelector(".block");
+const blockAnimation = document.querySelector(".animation");
+const btnStart = document.querySelector(".block_btn_start");
+const btnReset = document.querySelector(".block_btn_reset");
+const btnStop = document.querySelector(".block_btn_stop");
+
+let position = 0;
+let idInterval;
+let active = true;
+
+const squareAnimation = () => {
+  position++;
+
+  idInterval = requestAnimationFrame(squareAnimation);
+
+  if (position < 500) {
+    blockAnimation.style.top = position + "px";
+    blockAnimation.style.left = position + "px";
+  } else {
+    cancelAnimationFrame(idInterval);
+  }
+};
+
+const stopAndActiveAnimation = () => {
+  if (active) {
+    cancelAnimationFrame(idInterval);
+    active = false;
+  } else {
+    idInterval = requestAnimationFrame(squareAnimation);
+    active = true;
+  }
+};
+
+const resetAnimation = () => {
+  position = 0;
+  if (position === 0) {
+    blockAnimation.style.top = position + "px";
+    blockAnimation.style.left = position + "px";
+  }
+};
+
+btnStart.addEventListener("click", stopAndActiveAnimation);
+
+btnReset.addEventListener("click", resetAnimation);
